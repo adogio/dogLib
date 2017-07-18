3081b0668423f056afc7483c1f88706a5b2b1685
WMXPY <wm@wmpcxpy.com>
2017-07-18 16:37:36 -0500

    1.0.1

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # dogLib
+
 [![npm version](https://badge.fury.io/js/doglib.svg)](https://badge.fury.io/js/doglib)
 ![downloads](https://img.shields.io/npm/dm/doglib.svg)
 
 A dog multi-function library for JavaScript.
+
+# Introduction
+
+Dog ui is a muti function library  
+
+# Notice
+
+Qrcode is using code from Kazuhiko Arase. 
--- src/qrcode/qrcode.ts
+++ src/qrcode/qrcode.ts
@@ -1,22 +1,16 @@
 import { qrcode } from '../lib/qrcode';
-export default class qrgen {
-    private typeNumber;
-    private CorrectLevel;
-    public constructor() {
-        this.typeNumber = 4;
-        this.CorrectLevel = 'L';
-    }
 
-    public toBase64(text, size) {
-        var qr = qrcode(this.typeNumber, this.CorrectLevel);
+export default function (text, size) {
+    let typeNumber = 4;
+    let CorrectLevel = 'L';
+    var base64 = toBase64(text, size);
+    var dataURL = 'data:image/gif;base64,' + base64;
+    function toBase64(text, size) {
+        var qr = qrcode(typeNumber, CorrectLevel);
         qr.addData(text);
         qr.make();
         var base64 = qr.createImgBase64(size);
         return base64;
     }
-    public toDataURL(text, size) {
-        var base64 = this.toBase64(text, size);
-        var dataURL = 'data:image/gif;base64,' + base64;
-        return dataURL;
-    }
+    return dataURL;
 }

546f4accbd4727c165a6caaa68125bf3354920eb
WMXPY <wm@wmpcxpy.com>
2017-07-18 21:23:20 -0500

    update

--- src/lib/qrcode.js
+++ src/lib/qrcode.js
@@ -289,31 +289,13 @@ exports.qrcode = function () {
         _this.createTableTag = function (cellSize, margin) {
             cellSize = cellSize || 2;
             margin = (typeof margin == 'undefined') ? cellSize * 4 : margin;
-            var qrHtml = '';
-            qrHtml += '<table style="';
-            qrHtml += ' border-width: 0px; border-style: none;';
-            qrHtml += ' border-collapse: collapse;';
-            qrHtml += ' padding: 0px; margin: ' + margin + 'px;';
-            qrHtml += '">';
-            qrHtml += '<tbody>';
+            var qrHtml = [];
             for (var r = 0; r < _this.getModuleCount(); r += 1) {
-                qrHtml += '<tr>';
+                qrHtml.push([]);
                 for (var c = 0; c < _this.getModuleCount(); c += 1) {
-                    qrHtml += '<td style="';
-                    qrHtml += ' border-width: 0px; border-style: none;';
-                    qrHtml += ' border-collapse: collapse;';
-                    qrHtml += ' padding: 0px; margin: 0px;';
-                    qrHtml += ' width: ' + cellSize + 'px;';
-                    qrHtml += ' height: ' + cellSize + 'px;';
-                    qrHtml += ' background-color: ';
-                    qrHtml += _this.isDark(r, c) ? '#000000' : '#ffffff';
-                    qrHtml += ';';
-                    qrHtml += '"/>';
+                    qrHtml[r].push(_this.isDark(r, c) ? 1 : 0);
                 }
-                qrHtml += '</tr>';
             }
-            qrHtml += '</tbody>';
-            qrHtml += '</table>';
             return qrHtml;
         };
         _this.createImgTag = function (cellSize, margin) {
--- src/lib/qrcode.ts
+++ src/lib/qrcode.ts
@@ -411,37 +411,30 @@ export var qrcode = function () {
             cellSize = cellSize || 2;
             margin = (typeof margin == 'undefined') ? cellSize * 4 : margin;
 
-            var qrHtml = '';
-
-            qrHtml += '<table style="';
-            qrHtml += ' border-width: 0px; border-style: none;';
-            qrHtml += ' border-collapse: collapse;';
-            qrHtml += ' padding: 0px; margin: ' + margin + 'px;';
-            qrHtml += '">';
-            qrHtml += '<tbody>';
+            var qrHtml = [];
 
             for (var r = 0; r < _this.getModuleCount(); r += 1) {
 
-                qrHtml += '<tr>';
+                qrHtml.push([]);
 
                 for (var c = 0; c < _this.getModuleCount(); c += 1) {
-                    qrHtml += '<td style="';
-                    qrHtml += ' border-width: 0px; border-style: none;';
-                    qrHtml += ' border-collapse: collapse;';
-                    qrHtml += ' padding: 0px; margin: 0px;';
-                    qrHtml += ' width: ' + cellSize + 'px;';
-                    qrHtml += ' height: ' + cellSize + 'px;';
-                    qrHtml += ' background-color: ';
-                    qrHtml += _this.isDark(r, c) ? '#000000' : '#ffffff';
-                    qrHtml += ';';
-                    qrHtml += '"/>';
+                    qrHtml[r].push(_this.isDark(r, c) ? 1 : 0);
+                    // qrHtml += '<td style="';
+                    // qrHtml += ' border-width: 0px; border-style: none;';
+                    // qrHtml += ' border-collapse: collapse;';
+                    // qrHtml += ' padding: 0px; margin: 0px;';
+                    // qrHtml += ' width: ' + cellSize + 'px;';
+                    // qrHtml += ' height: ' + cellSize + 'px;';
+                    // qrHtml += ' background-color: ';
+                    // qrHtml += _this.isDark(r, c) ? '#000000' : '#ffffff';
+                    // qrHtml += ';';
+                    // qrHtml += '"/>';
                 }
 
-                qrHtml += '</tr>';
+                // qrHtml += '</tr>';
             }
 
-            qrHtml += '</tbody>';
-            qrHtml += '</table>';
+            // qrHtml += '</tbody>';
 
             return qrHtml;
         };
